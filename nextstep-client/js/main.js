08caef6f90ad86d63bda5e93120535216322a6d2
prajogotio <ggkgaya@hotmail.com>
2015-11-07 21:51:10 +0000

    changed lobby music background [credit: ccodes chen chin jieh]

--- nextstep-client/js/main.js
+++ nextstep-client/js/main.js
@@ -3073,17 +3073,12 @@
 
 		// audio
 		asset["lobby"] = {
-			start : new Audio('asset/lobby_start.mp3'),
-			main : new Audio('asset/lobby_main.mp3'),
+			main : new Audio('asset/lobby_long.mp3'),
 			play : function() {
-				this.start.volume = 0.09;
-				this.main.volume = 0.09;
-				this.start.play();
+				this.main.volume = 0.21;
+				this.main.play();
 			}
 		}
-		asset["lobby"].start.addEventListener('ended', function(){
-			asset["lobby"].main.play();
-		});
 		asset["lobby"].main.addEventListener('ended', function() {
 			this.currentTime = 0;
 			this.play();
